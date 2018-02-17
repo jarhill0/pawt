26b2e8c4a6c1106692343e97fa7bbbba9bad3045
jarhill0 <jarhill0@users.noreply.github.com>
2018-02-16 16:31:58 -0800

    Start writing tests

--- setup.py
+++ setup.py
@@ -5,5 +5,8 @@ from pawt import __version__
 setup(name='pawt',
       description='Pre-alpha Telegram API wrapper',
       install_requires=['requests >= 2.18.4'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      test_suite='tests',
       packages=find_packages(exclude=['tests', 'tests.*']),
       version=__version__)
---
+++ tests/unit/test_telegram.py
@@ -0,0 +1,40 @@
+from pawt import Telegram
+from pawt.models import Chat, File, StickerSet, User
+
+DUMMY_TOKEN = '542097629:AAGxYuGjLUWR7X1xf1x3kszU7DjRSCNE9VU'
+
+
+def test_creation():
+    tg = Telegram(DUMMY_TOKEN)
+
+
+def test_copy():
+    tg = Telegram(DUMMY_TOKEN)
+    copy = tg.copy()
+
+    assert tg is not copy
+    assert tg.token == copy.token
+
+
+def test_alt_url():
+    url = 'https://altelapi.com/bot/{token}'  # without trailing slash
+    tg = Telegram(DUMMY_TOKEN, url=url)
+
+    assert tg.path == tg.copy().path
+
+
+def test_return_types():
+    dummy_id = 23456
+    tg = Telegram(DUMMY_TOKEN)
+
+    chat = tg.chat(dummy_id)
+    assert isinstance(chat, Chat)
+
+    file = tg.file(dummy_id)
+    assert isinstance(file, File)
+
+    sticker_set = tg.sticker_set('set name')
+    assert isinstance(sticker_set, StickerSet)
+
+    user = tg.user(dummy_id)
+    assert isinstance(user, User)

5e405441d04bc049d2f30b2247d4121215a8ece0
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-08 10:57:27 -0800

    Change behavior of Input Message Content to exclude only None

--- pawt/models/input_message_content/input_message_content.py
+++ pawt/models/input_message_content/input_message_content.py
@@ -2,6 +2,6 @@ class InputMessageContent:
     def to_dict(self):
         out = dict()
         for key, val in self.__dict__.items():
-            if not key.startswith('_') and val:
+            if not key.startswith('_') and val is not None:
                 out[key] = val
         return out

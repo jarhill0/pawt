a4cde5a9a01498975a495f6979b0e7047c8cdd3e
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-08 09:42:26 -0800

    Add more tests

--- tests/unit/models/message_specials/test_message_entity.py
+++ tests/unit/models/message_specials/test_message_entity.py
@@ -1,18 +1,20 @@
 from pawt import Telegram
-from pawt.models.message_specials import Mention, Hashtag, BotCommand, Url, \
-    Email, Bold, Italic, Code, Pre, TextLink, TextMention
+from pawt.models.message_specials import Bold, BotCommand, Code, Email, Hashtag, \
+    Italic, Mention, Pre, TextLink, TextMention, Url
 
 dummy_tg = Telegram(token='')
+
 
 # todo test the rest of the classes
 
 def test_mention():
-    data = {'offset': 3, 'length':12}
+    data = {'offset': 3, 'length': 12}
     text = 'Hi Ben Franklin!'
     m = Mention(dummy_tg, data, text)
     assert m.content == 'Ben Franklin'
     assert str(m) == m.content
     assert repr(m) == '<Mention: Ben Franklin>'
+
 
 def test_hashtag():
     data = {'offset': 6, 'length': 4}
@@ -21,6 +23,7 @@ def test_hashtag():
     assert h.content == '#lit'
     assert str(h) == h.content
     assert repr(h) == '<Hashtag: #lit>'
+
 
 def test_botcommand():
     data = {'offset': 3, 'length': 10}
@@ -32,6 +35,7 @@ def test_botcommand():
 
     assert bc.command == '/go'
 
+
 def test_url():
     data = {'offset': 6, 'length': 22}
     text = 'Go to https://duckduckgo.com now!'
@@ -39,6 +43,7 @@ def test_url():
     assert u.content == 'https://duckduckgo.com'
     assert str(u) == u.content
     assert repr(u) == '<Url: https://duckduckgo.com>'
+
 
 def test_email():
     data = {'offset': 12, 'length': 9}
@@ -49,6 +54,59 @@ def test_email():
     assert repr(e) == '<Email: hi@hi.com>'
 
 
-def test_equality():
-    pass
+def test_bold():
+    data = {'offset': 10, 'length': 4}
+    text = 'This test must pass.'
+    b = Bold(dummy_tg, data, text)
+    assert b.content == 'must'
+    assert str(b) == b.content
+    assert repr(b) == '<Bold: must>'
 
+
+def test_italic():
+    data = {'offset': 2, 'length': 4}
+    text = 'I love this test.'
+    i = Italic(dummy_tg, data, text)
+    assert i.content == 'love'
+    assert str(i) == i.content
+    assert repr(i) == '<Italic: love>'
+
+
+def test_code():
+    data = {'offset': 4, 'length': 21}
+    text = 'Use python3 setup.py test to run these tests.'
+    c = Code(dummy_tg, data, text)
+    assert c.content == 'python3 setup.py test'
+    assert str(c) == c.content
+    assert repr(c) == '<Code: python3 setup.py test>'
+
+
+def test_pre():
+    data = {'offset': 5, 'length': 10}
+    text = 'um,\n\nWUT\nUU\nT T\n\n…'
+    p = Pre(dummy_tg, data, text)
+    assert p.content == 'WUT\nUU\nT T'
+    assert str(p) == p.content
+    assert repr(p) == '<Pre: WUT\nUU\nT T>'
+
+
+def test_textlink():
+    data = {'offset': 6, 'length': 4, 'url': 'https://duckduckgo.com'}
+    text = 'Click here to search.'
+    tl = TextLink(dummy_tg, data, text)
+    assert tl.content == 'here'
+    assert tl.url == 'https://duckduckgo.com'
+    assert str(tl) == tl.content
+    assert repr(tl) == '<TextLink: here>'
+
+
+def test_textmention():
+    data = {'offset': 6, 'length': 4, 'user': {'id': 123456789,
+                                               'is_bot': False,
+                                               'first_name': 'Sally'}}
+    text = 'Click here to view my profile.'
+    tm = TextMention(dummy_tg, data, text)
+    assert tm.content == 'here'
+    assert tm.user == 123456789
+    assert str(tm) == tm.content
+    assert repr(tm) == '<TextMention: here>'

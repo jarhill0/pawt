4df6cf502c5eb862e78279eef4bd65c20c266991
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-08 12:43:33 -0800

    Fix to_dict

--- pawt/models/inline_queries/inline_query_result.py
+++ pawt/models/inline_queries/inline_query_result.py
@@ -9,7 +9,7 @@ class InlineQueryResult:
 
     def to_dict(self):
         out = dict()
-        for key, val in self.__dict__:
-            if not key.startswith('_') and val:
+        for key, val in self.__dict__.items():
+            if not key.startswith('_') and val is not None:
                 out[key] = val
         return out

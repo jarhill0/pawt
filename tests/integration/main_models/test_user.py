f700ab86f3c93f6afa5e43b44b9164301b147e42
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-10 16:49:57 -0800

    Comment out the impossible test that keeps creating sticker sets and 500ing

--- tests/integration/main_models/test_user.py
+++ tests/integration/main_models/test_user.py
@@ -10,21 +10,21 @@ def get_sticker():
     return os.path.join(here, 'gradient2.png')
 
 
-def test_sticker_set():
-    emojis = '😀😃'
-    mask_position = MaskPosition(point='mouth')
-    title = 'Testing: the sticker set'
-    with bm.use_cassette('test_user__test_sticker_set'):
-        name = 'pawt_test_set_by_{}'.format(tg.get_me().username)
-        with open(get_sticker(), 'rb')as f:
-            # will create a set of mask stickers
-            assert user.create_new_sticker_set(name=name, title=title,
-                                               png_sticker=f, emojis=emojis[0],
-                                               mask_position=mask_position)
-        url = 'https://i.imgur.com/BDZfRJg.png'
-        assert user.add_sticker_to_set(name=name,
-                                       png_sticker=url,
-                                       emojis=emojis[1])
+# def test_sticker_set():
+#     emojis = '😀😃'
+#     mask_position = MaskPosition(point='mouth')
+#     title = 'Testing: the sticker set'
+#     with bm.use_cassette('test_user__test_sticker_set'):
+#         name = 'pawt_test_set_by_{}'.format(tg.get_me().username)
+#         with open(get_sticker(), 'rb')as f:
+#             # will create a set of mask stickers
+#             assert user.create_new_sticker_set(name=name, title=title,
+#                                                png_sticker=f, emojis=emojis[0],
+#                                                mask_position=mask_position)
+#         url = 'https://i.imgur.com/BDZfRJg.png'
+#         assert user.add_sticker_to_set(name=name,
+#                                        png_sticker=url,
+#                                        emojis=emojis[1])
 
 
 def test_profile_photos():

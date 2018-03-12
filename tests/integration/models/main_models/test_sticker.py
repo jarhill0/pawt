097166e7103174a0e3bb5a5df42d55bd798a96b8
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-12 15:15:55 -0700

    Increase Sticker test coverage (to 100%?)

---
+++ tests/integration/models/main_models/test_sticker.py
@@ -0,0 +1,30 @@
+from ... import bm, tg, user
+
+user = tg.user(user)
+
+
+def get_sticker():
+    import os
+    here = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(here, 'gradient2.png')
+
+
+def test_sticker():
+    name = 'pawt_sticker_misc_testing_by_' + tg.get_me().username
+    with bm.use_cassette('test_sticker__test_sticker'):
+        with open(get_sticker(), 'rb') as f:
+            user.create_new_sticker_set(name, 'test_sticker.py', f, '🙃')
+        user.add_sticker_to_set(name, 'https://i.imgur.com/BDZfRJg.png', '🍊')
+
+        sticker_set = tg.sticker_set(name)
+        assert sticker_set == sticker_set.stickers[0].get_set()
+        stickers_orig = sticker_set.stickers
+        stickers_orig[1].set_position(0)
+        stickers_new = tg.sticker_set(name).stickers
+
+        assert stickers_orig == stickers_new[1:] + stickers_new[:1]
+
+        assert user.chat.send_sticker(stickers_new[0])
+
+        stickers_new[0].delete()
+        assert len(tg.sticker_set(name).stickers) == 1
--- tests/unit/models/main_models/test_sticker.py
+++ tests/unit/models/main_models/test_sticker.py
@@ -1,6 +1,6 @@
 from pawt import Telegram
 from pawt.exceptions import BadArgument
-from pawt.models import Sticker
+from pawt.models import MaskPosition, Sticker
 
 TG = Telegram('')
 
@@ -11,7 +11,7 @@ def test_attrs():
     assert sticker.height == 510
 
 
-def test_mask_pos():
+def test_mask_position_validation():
     mask_position = dict(point='forehead', x_shift=-1.5,
                          y_shift=2 / 3, scale=2.0)
     data = dict(width=512, height=510, file_id=12345,
@@ -27,6 +27,20 @@ def test_mask_pos():
         pass
     else:
         assert False
+
+
+def test_mask_position():
+    try:
+        mp = MaskPosition({'a': 1}, 13)
+        assert False, 'should raise BadArgument'
+    except BadArgument:
+        pass
+
+    try:
+        mp = MaskPosition()
+        assert False, 'should raise BadArgument'
+    except BadArgument:
+        pass
 
 
 def test_repr():

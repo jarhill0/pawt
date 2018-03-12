c664cb0cfd3297d9e93dae9b469414134fa9d5b2
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-12 15:39:17 -0700

    Increase StickerSet coverage

---
+++ tests/integration/models/main_models/test_sticker_set.py
@@ -0,0 +1,34 @@
+from pawt.models import StickerSet
+from ... import bm, tg, user
+
+user = tg.user(user)
+
+
+def get_sticker():
+    import os
+    here = os.path.abspath(os.path.dirname(__file__))
+    return os.path.join(here, 'gradient2.png')
+
+
+def test_sticker_set():
+    with bm.use_cassette('test_sticker_set__test_sticker_set'):
+        name = 'pawt_test_sticker_set_by_' + tg.get_me().username
+        with open(get_sticker(), 'rb') as f:
+            user.create_new_sticker_set(name, 'test_sticker_set.py', f, '🙃')
+
+        sticker_set = tg.sticker_set(name)
+
+        user.add_sticker_to_set(name, 'https://i.imgur.com/BDZfRJg.png', '🍊')
+
+    assert str(sticker_set) == 'test_sticker_set.py'
+    assert repr(sticker_set).startswith(
+        '<StickerSet pawt_test_sticker_set_by_')
+
+
+def test_objectify_from_data():
+    with bm.use_cassette('test_sticker_set__test_objectify_from_data'):
+        data = tg.get('getStickerSet', params={'name': 'LazyPanda'})
+        sticker_set = StickerSet(tg, data=data)
+    assert sticker_set.name == 'LazyPanda'
+    assert sticker_set.title == 'Lazy Panda'
+    assert len(sticker_set.stickers) > 5

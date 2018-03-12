ff517dc24067f481a519e61711c7e49a468b98d6
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-12 11:39:00 -0700

    Write test for Telegram.get_updates()

--- tests/integration/test_telegram.py
+++ tests/integration/test_telegram.py
@@ -1,7 +1,19 @@
 from . import bm, tg
 
+
 def test_get_me():
     with bm.use_cassette('test_telegram__test_get_me'):
         me = tg.get_me()
         assert me.is_bot
         assert me == tg.get_me()
+
+
+def test_get_updates():
+    with bm.use_cassette('test_telegram__test_get_updates'):
+        updates = tg.get_updates(limit=2, allowed_updates=['message'])
+        assert len(updates) == 2
+        offset = 0
+        for update in updates:
+            assert update.message
+            offset = max(offset, update.id)
+        tg.get_updates(offset=offset + 1, timeout=5)

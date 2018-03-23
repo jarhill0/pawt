a35c01998dde402b34b0dd19ef50472ac4ab88bb
jarhill0 <jarhill0@users.noreply.github.com>
2018-03-22 21:56:36 -0700

    Bump version to 0.0.2

--- pawt/__init__.py
+++ pawt/__init__.py
@@ -7,4 +7,4 @@ from .models.reply_markup import InlineKeyboardMarkupBuilder, \
     ReplyKeyboardMarkupBuilder, force_reply, reply_keyboard_remove
 from .telegram import Telegram
 
-__version__ = '0.0.1a19'
+__version__ = '0.0.2a1'
